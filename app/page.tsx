6ac0981f851cba3e25a571178d6c0bd4249d5b26
zesidor <106875614+zesidor@users.noreply.github.com>
2026-08-27 14:21:48 +0000

    082726421

--- app/page.tsx
+++ app/page.tsx
@@ -16,9 +16,9 @@ export default function Home() {
         <section className="w-full max-w-md flex flex-col gap-6  p-6 ">
           {!token ? (
             <>
-             {/* <h2 className="text-lg font-semibold">Register</h2>
+              <h2 className="text-lg font-semibold">Register</h2>
               <RegisterForm />
-              <h2 className="text-lg font-semibold">By wejść w te progi oddaj chołd Prezesowi</h2>*/}
+              <h2 className="text-lg font-semibold">By wejść w te progi oddaj chołd Prezesowi</h2>
               <LoginForm />
             </>
           ) : (
